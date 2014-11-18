59a0f51d591df62c0a0a300dfbe04a73b3c80e86
Vladimir Grachev <vladimir.grachev@booking.com>
2014-11-18 17:10:01 +0100

    base DTOs

---
+++ app/src/main/java/com/vgrachev/data/Booking.java
@@ -0,0 +1,14 @@
+package com.vgrachev.data;
+
+import java.util.List;
+
+public class Booking {
+
+    int id;
+    int hotelId;
+
+    String checkin;
+    String checkout;
+
+    List<Room> rooms;
+}
---
+++ app/src/main/java/com/vgrachev/data/Room.java
@@ -0,0 +1,9 @@
+package com.vgrachev.data;
+
+public class Room {
+
+    String reservation_id;
+    String price;
+    String extraCharges;
+
+}
